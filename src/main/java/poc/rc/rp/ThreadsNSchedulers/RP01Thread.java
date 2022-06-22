0648f3f11f227917999366e8fc49e6a1fd1ca65a
Ramcharan <ramcharan.adapa@cdk.com>
2022-06-22 23:54:50 +0530

    Threads and Schedulers

---
+++ src/main/java/poc/rc/rp/ThreadsNSchedulers/RP01Thread.java
@@ -0,0 +1,38 @@
+package poc.rc.rp.ThreadsNSchedulers;
+
+import poc.rc.rp.commonutils.Util;
+import reactor.core.publisher.Flux;
+
+public class RP01Thread {
+
+  public static void main(String[] args) {
+
+    /**
+     * Default behavior is everything runs on Main Thread
+     */
+    Flux<Object> flux = Flux.create(fluxSink -> {
+      printCurrThread("create");
+      fluxSink.next(1);
+    })
+        .doOnNext(i -> printCurrThread("next: " + i));
+
+//    flux.subscribe(v -> printCurrThread("subscriber: " + v));
+
+    /**
+     * Alternately we can run on different threads but should be managed
+     * so we use scheduler methods provided by reactor
+     */
+    Runnable runnable = () -> flux.subscribe(v -> printCurrThread("sub " + v));
+
+    for (int i = 0; i < 2; i++) {
+      new Thread(runnable).start();
+    }
+
+    Util.sleepSeconds(5);
+
+  }
+
+  private static void printCurrThread(String msg) {
+    System.out.println(msg + "\t\t Thread: " + Thread.currentThread().getName());
+  }
+}
---
+++ src/main/java/poc/rc/rp/ThreadsNSchedulers/RP03SubscribeOnMultipleItems.java
@@ -0,0 +1,35 @@
+package poc.rc.rp.ThreadsNSchedulers;
+
+import poc.rc.rp.commonutils.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class RP03SubscribeOnMultipleItems {
+
+  public static void main(String[] args) {
+
+    Flux<Object> flux = Flux.create(fluxSink -> {
+      printCurrThread("create");
+      for (int i = 0; i < 20; i++) {
+        fluxSink.next(i);
+      }
+      fluxSink.complete();
+    })
+        .doOnNext(i -> printCurrThread("next: " + i));
+
+    flux
+        .subscribeOn(Schedulers.boundedElastic())
+        .subscribe(v -> printCurrThread("subscriber1: " + v));
+
+    flux
+        .subscribeOn(Schedulers.parallel())
+        .subscribe(v -> printCurrThread("subscriber2: " + v));
+
+    Util.sleepSeconds(5);
+
+  }
+
+  private static void printCurrThread(String msg) {
+    System.out.println(msg + "\t\t Thread: " + Thread.currentThread().getName());
+  }
+}
